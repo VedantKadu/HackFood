a5a8b543ba34b71a9360825dd214ce7e85b8631c
Prakhar Agrawal <prakharagrawal2408@gmail.com>
2021-11-14 00:11:17 +0530

    Fetching Restaurant list logic added

--- src/pages/customer/home/HomePage.js
+++ src/pages/customer/home/HomePage.js
@@ -1,53 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import HomePageItem from "../../../components/customers/layout/home/home-items/HomePageItem";
+import { RestaurentDataActions } from "../../../store/RestaurentData-slice";
 
 import styles from "./HomePage.module.css";
 
 const HomePage = () => {
-  const item = {
-    id: "m1",
-    name: "Mutton Curry",
-    price: 300,
-    discount: 20,
-    veg: false,
-    url: "https://www.thespruceeats.com/thmb/kFPbPBvit_2pnmUt8lhABceAM-I=/3329x3329/smart/filters:no_upscale()/goat-mutton-curry-1957594-hero-01-afaf638173cd47d595c7ad99a018cf01.jpg",
-    description: "",
-    hotel_name: "Kerla Restaurent",
-  };
+  const dispatch = useDispatch();
+  const restaurentList = useSelector((state) => state.restaurent.hotels);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/customer/", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("customerToken"),
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Failed to fetch products.");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        dispatch(RestaurentDataActions.changeData(resData.hotels));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [dispatch]);
+
   return (
     <div className={styles.home_wrapper}>
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
-      <HomePageItem item={item} />
+      {restaurentList.length > 0 &&
+        restaurentList.map((restaurent) => {
+          return <HomePageItem item={restaurent} />;
+        })}
     </div>
   );
 };
